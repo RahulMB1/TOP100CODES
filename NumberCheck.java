4a370a2d6604d057fb4cfa571b9e8d9150cc83e7
RahulMB1 <rahulbashetti12@gmail.com>
2023-09-28 21:57:35 +0530

    TOP100CODE

---
+++ NumberCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println("Positive Number");
+        }
+
+        if (num < 0) {
+            System.out.println("Negative Number");
+        }
+    }
+
+}
